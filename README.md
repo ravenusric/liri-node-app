8642a6c3f6321e9cbbf58cdd8006fa6c4dfe9701
ravenusric <richie.roney@gmail.com>
2018-06-28 22:44:30 -0400

    added files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # liri-node-app
+
+This is a Liri node application that is similar to Apple's Siri. Liri is a language interpretation and recognition interface. Liri is a command line node application that takes in parameters and sends back data.
+
+to install these npm packages run these commands one at a time.
+
+npm install twitter
+npm install spotify
+npm install request
+
+*Commands to run LIRI
+
+Follow the format presented in these queries
+
+* node liri.js my-tweets
+* node liri.js spotify-this-song '<song name here>'
+* node liri.js movie-this '<movie name here>'
+* node liri.js do-what-it-says 
---
+++ liri.js
@@ -0,0 +1,160 @@
+// read and set any environment variables with the dotenv
+require("dotenv").config();
+//Liri takes the following arguments
+// * my-tweets
+// * spotify-this-song
+// * movie-this
+// * do-what-it-says
+//these add other programs to this one
+var dataKeys = require("./keys.js");
+var fs = require('fs'); //file system
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+var writeToLog = function(data) {
+  fs.appendFile("log.txt", '\r\n\r\n');
+
+  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("log.txt was updated!");
+  });
+}
+
+// getMeSpotify function
+//Creates a function for finding artist name from spotify
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+
+//Function for finding songs on Spotify
+var getMeSpotify = function(songName) {
+  //If it doesn't find a song, find Blink 182's What's my age again
+  if (songName === undefined) {
+    songName = 'What\'s my age again';
+  };
+
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
+
+    var songs = data.tracks.items;
+    var data = []; //empty array to hold data
+
+    for (var i = 0; i < songs.length; i++) {
+      data.push({
+        'artist(s)': songs[i].artists.map(getArtistNames),
+        'song name: ': songs[i].name,
+        'preview song: ': songs[i].preview_url,
+        'album: ': songs[i].album.name,
+      });
+    }
+    console.log(data);
+    writeToLog(data);
+  });
+};
+
+// Twitter function
+// Function to show my last 20 tweets.
+function getTweets() {
+	
+	// Passes Twitter keys into call to Twitter API.
+	var client = new twitter(dataKeys.twitterKeys);
+
+	// Search parameters includes my tweets up to last 20 tweets;
+	var params = {q: '@ravishin_rich', count: 20};
+
+	// Shows up to last 20 tweets and when created in terminal.
+	client.get('search/tweets', params, function(error, tweets, response) {
+	  if (!error) {
+
+	  	// Loops through tweets and prints out tweet text and creation date.
+	  	for (var i = 0; i < tweets.statuses.length; i++) {
+	  		var tweetText = tweets.statuses[i].text;
+	  		logOutput("Tweet Text: " + tweetText);
+	  		var tweetCreationDate = tweets.statuses[i].created_at;
+	  		logOutput("Tweet Creation Date: " + tweetCreationDate);
+	  	}
+	  } else {
+	  	logOutput(error);
+	  }
+	});
+}
+var getMeMovie = function(movieName) {
+
+  if (movieName === undefined) {
+    movieName = 'Mr Nobody';
+  }
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'Rated: ' : jsonData.Rated,
+      'IMDB Rating: ' : jsonData.imdbRating,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language,
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
+      'Rotton Tomatoes URL: ' : jsonData.tomatoURL,
+  });
+      console.log(data);
+      writeToLog(data);
+}
+  });
+
+}
+
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    writeToLog(data);
+    var dataArr = data.split(',')
+
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+
+  });
+}
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      getTweets();
+      break;
+    case 'spotify-this-song':
+      getMeSpotify(functionData);
+      break;
+    case 'movie-this':
+      getMeMovie(functionData);
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI doesn\'t know that');
+  }
+}
+
+//run this on load of js file
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
